edc1a44be5a6c1e7e3ec59c5cf7c2cb2d631f4a7
tanodam <desariod95@gmail.com>
2020-04-02 20:43:18 -0300

    mas perdido que turco en la neblina

---
+++ myApp Clase 2/src/app/componentes/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../clases/usuario';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private route: Router) { }
+  
+
+  ngOnInit(): void {
+  }
+
+  usuario = new Usuario();
+  Ingresar() {
+    console.log(this.usuario);
+    if(this.usuario.email === "admin@mail.com" && this.usuario.pass === "1234")
+    {
+      this.route.navigate(["home"]);
+      console.log("OK");
+    }
+    else
+    {
+      this.route.navigate(["error"]);
+      console.log("NO");
+    }
+  }
+
+}
